1f3d8c3c9a241b7aaba9fb7f975fb80e55b407a3
sunamo.cz <radek.jancik@sunamo.cz>
2024-08-09 14:40:19 +0200

    Trim XlfKeys

--- CAIndexesWithNull.cs
+++ CAIndexesWithNull.cs
@@ -3,23 +3,18 @@ namespace SunamoCollectionsIndexesWithNull;
 public class CAIndexesWithNull
 {
     /// <summary>
-    /// Dont trim
+    ///     Dont trim
     /// </summary>
     /// <param name="times"></param>
     public static List<int> IndexesWithNullOrEmpty(IList times)
     {
-        List<int> nulled = new List<int>();
-        int i = 0;
+        var nulled = new List<int>();
+        var i = 0;
         foreach (var item in times)
         {
             if (item == null)
-            {
                 nulled.Add(i);
-            }
-            else if (item.ToString() == string.Empty)
-            {
-                nulled.Add(i);
-            }
+            else if (item.ToString() == string.Empty) nulled.Add(i);
             i++;
         }
 
@@ -27,24 +22,19 @@ public class CAIndexesWithNull
     }
 
     /// <summary>
-    /// For all types
+    ///     For all types
     /// </summary>
     /// <param name="times"></param>
     public static List<int> IndexesWithNull(IList times)
     {
-        List<int> nulled = new List<int>();
-        int i = 0;
+        var nulled = new List<int>();
+        var i = 0;
         foreach (var item in times)
         {
-            if (item == null)
-            {
-                nulled.Add(i);
-            }
+            if (item == null) nulled.Add(i);
             i++;
         }
 
         return nulled;
     }
-
-
 }
