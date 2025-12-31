0444f413333e0b899e9beb15f211af1c999da273
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 01:04:55 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoCollectionsIndexesWithNull/_sunamo/FS.cs
+++
@@ -1,8 +0,0 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoCollectionsIndexesWithNull._sunamo;
-
-//namespace SunamoCollectionsIndexesWithNull._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FS
-{
-}
