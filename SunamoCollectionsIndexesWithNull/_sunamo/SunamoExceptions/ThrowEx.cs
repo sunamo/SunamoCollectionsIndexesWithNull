36342d336fe9ea57588be53bebd6dbdfbab4733f
smutekutek <smutekutek@gmail.com>
2026-06-24 22:32:03 +0200

    Ahoj! Jak ti mohu pomoci?

--- SunamoCollectionsIndexesWithNull/_sunamo/SunamoExceptions/ThrowEx.cs
+++ SunamoCollectionsIndexesWithNull/_sunamo/SunamoExceptions/ThrowEx.cs
@@ -1,9 +1,6 @@
 // variables names: ok
 namespace SunamoCollectionsIndexesWithNull._sunamo.SunamoExceptions;
 
-/// <summary>
-/// Provides shared exception throwing utilities.
-/// </summary>
 internal partial class ThrowEx
 {
 }
